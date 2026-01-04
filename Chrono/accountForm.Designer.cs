bf5bd63636fa0fd72dae967dcf4517510172a1c6
Sed <hesedmatthreyes@gmail.com>
2026-01-04 11:33:21 +0800

    Add files via upload
    Gais eto UPDATED "Chrono" folder ni Hesed.
    
    Dashboard update. New window form for Login & Register.

---
+++ Chrono/accountForm.Designer.cs
@@ -0,0 +1,102 @@
+﻿namespace Chrono
+{
+    partial class accountForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 18.2F, System.Drawing.FontStyle.Bold);
+            this.label1.Location = new System.Drawing.Point(497, 43);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(121, 35);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Chrono";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.7F);
+            this.label2.ForeColor = System.Drawing.Color.DimGray;
+            this.label2.Location = new System.Drawing.Point(504, 80);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(171, 16);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Stay on Task, Stay on Time";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.White;
+            this.panel1.Location = new System.Drawing.Point(330, 112);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(450, 537);
+            this.panel1.TabIndex = 3;
+            this.panel1.Paint += new System.Windows.Forms.PaintEventHandler(this.panel1_Paint);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Century Gothic", 30F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(445, 37);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(70, 61);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "O";
+            // 
+            // accountForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.GhostWhite;
+            this.ClientSize = new System.Drawing.Size(1104, 715);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.label3);
+            this.Name = "accountForm";
+            this.Text = "accountForm";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label3;
+    }
+}
--- Chrono/dashBoardControl.cs
+++ Chrono/dashBoardControl.cs
@@ -16,5 +16,20 @@ namespace Chrono
         {
             InitializeComponent();
         }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label5_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label16_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
