7cb86e4b614096fefff25b03168014f2c875d10a
WenjaBoii <sir.rickraven14@gmail.com>
2026-01-05 17:01:44 +0800

    tips Gui update

--- Chrono/tipsControl.Designer.cs
+++ Chrono/tipsControl.Designer.cs
@@ -36,26 +36,12 @@
             this.panel6 = new System.Windows.Forms.Panel();
             this.label20 = new System.Windows.Forms.Label();
             this.pictureBox5 = new System.Windows.Forms.PictureBox();
-            this.panel1 = new System.Windows.Forms.Panel();
-            this.notificationText = new System.Windows.Forms.Label();
-            this.pictureBox7 = new System.Windows.Forms.PictureBox();
-            this.panel4 = new System.Windows.Forms.Panel();
-            this.buttonUnread = new System.Windows.Forms.Button();
-            this.allButton = new System.Windows.Forms.Button();
-            this.panel3 = new System.Windows.Forms.Panel();
-            this.pictureBox6 = new System.Windows.Forms.PictureBox();
-            this.label21 = new System.Windows.Forms.Label();
-            this.panel10 = new System.Windows.Forms.Panel();
-            this.label22 = new System.Windows.Forms.Label();
             this.label23 = new System.Windows.Forms.Label();
+            this.label22 = new System.Windows.Forms.Label();
+            this.panel10 = new System.Windows.Forms.Panel();
             this.tipsPanel.SuspendLayout();
             this.flowLayoutPanel1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).BeginInit();
-            this.panel1.SuspendLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox7)).BeginInit();
-            this.panel4.SuspendLayout();
-            this.panel3.SuspendLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox6)).BeginInit();
             this.panel10.SuspendLayout();
             this.SuspendLayout();
             // 
@@ -65,12 +51,10 @@
             this.tipsPanel.Controls.Add(this.flowLayoutPanel1);
             this.tipsPanel.Controls.Add(this.label20);
             this.tipsPanel.Controls.Add(this.pictureBox5);
-            this.tipsPanel.Controls.Add(this.panel1);
             this.tipsPanel.Controls.Add(this.panel10);
-            this.tipsPanel.Location = new System.Drawing.Point(4, 4);
-            this.tipsPanel.Margin = new System.Windows.Forms.Padding(4);
+            this.tipsPanel.Location = new System.Drawing.Point(3, 3);
             this.tipsPanel.Name = "tipsPanel";
-            this.tipsPanel.Size = new System.Drawing.Size(811, 806);
+            this.tipsPanel.Size = new System.Drawing.Size(595, 485);
             this.tipsPanel.TabIndex = 30;
             // 
             // flowLayoutPanel1
@@ -79,218 +63,101 @@
             this.flowLayoutPanel1.Controls.Add(this.panel8);
             this.flowLayoutPanel1.Controls.Add(this.panel7);
             this.flowLayoutPanel1.Controls.Add(this.panel6);
-            this.flowLayoutPanel1.Location = new System.Drawing.Point(7, 447);
-            this.flowLayoutPanel1.Margin = new System.Windows.Forms.Padding(4);
+            this.flowLayoutPanel1.Location = new System.Drawing.Point(9, 102);
             this.flowLayoutPanel1.Name = "flowLayoutPanel1";
-            this.flowLayoutPanel1.Size = new System.Drawing.Size(776, 437);
+            this.flowLayoutPanel1.Size = new System.Drawing.Size(582, 355);
             this.flowLayoutPanel1.TabIndex = 4;
             // 
             // panel5
             // 
             this.panel5.BackColor = System.Drawing.Color.RoyalBlue;
-            this.panel5.Location = new System.Drawing.Point(4, 4);
-            this.panel5.Margin = new System.Windows.Forms.Padding(4);
+            this.panel5.Location = new System.Drawing.Point(3, 3);
             this.panel5.Name = "panel5";
-            this.panel5.Size = new System.Drawing.Size(363, 210);
+            this.panel5.Size = new System.Drawing.Size(272, 171);
             this.panel5.TabIndex = 0;
             // 
             // panel8
             // 
             this.panel8.BackColor = System.Drawing.Color.SteelBlue;
-            this.panel8.Location = new System.Drawing.Point(375, 4);
-            this.panel8.Margin = new System.Windows.Forms.Padding(4);
+            this.panel8.Location = new System.Drawing.Point(281, 3);
             this.panel8.Name = "panel8";
-            this.panel8.Size = new System.Drawing.Size(395, 210);
+            this.panel8.Size = new System.Drawing.Size(296, 171);
             this.panel8.TabIndex = 3;
             // 
             // panel7
             // 
             this.panel7.BackColor = System.Drawing.Color.CornflowerBlue;
-            this.panel7.Location = new System.Drawing.Point(4, 222);
-            this.panel7.Margin = new System.Windows.Forms.Padding(4);
+            this.panel7.Location = new System.Drawing.Point(3, 180);
             this.panel7.Name = "panel7";
-            this.panel7.Size = new System.Drawing.Size(363, 210);
+            this.panel7.Size = new System.Drawing.Size(272, 171);
             this.panel7.TabIndex = 2;
             // 
             // panel6
             // 
             this.panel6.BackColor = System.Drawing.Color.SkyBlue;
-            this.panel6.Location = new System.Drawing.Point(375, 222);
-            this.panel6.Margin = new System.Windows.Forms.Padding(4);
+            this.panel6.Location = new System.Drawing.Point(281, 180);
             this.panel6.Name = "panel6";
-            this.panel6.Size = new System.Drawing.Size(395, 210);
+            this.panel6.Size = new System.Drawing.Size(296, 171);
             this.panel6.TabIndex = 1;
             // 
             // label20
             // 
             this.label20.AutoSize = true;
             this.label20.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label20.Location = new System.Drawing.Point(63, 405);
-            this.label20.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label20.Location = new System.Drawing.Point(50, 67);
             this.label20.Name = "label20";
-            this.label20.Size = new System.Drawing.Size(207, 29);
+            this.label20.Size = new System.Drawing.Size(163, 24);
             this.label20.TabIndex = 3;
             this.label20.Text = "Productivity Tips";
             // 
             // pictureBox5
             // 
-            this.pictureBox5.Location = new System.Drawing.Point(7, 400);
-            this.pictureBox5.Margin = new System.Windows.Forms.Padding(4);
+            this.pictureBox5.Location = new System.Drawing.Point(8, 63);
             this.pictureBox5.Name = "pictureBox5";
-            this.pictureBox5.Size = new System.Drawing.Size(48, 41);
+            this.pictureBox5.Size = new System.Drawing.Size(36, 33);
             this.pictureBox5.TabIndex = 2;
             this.pictureBox5.TabStop = false;
-            // 
-            // panel1
-            // 
-            this.panel1.BackColor = System.Drawing.Color.White;
-            this.panel1.Controls.Add(this.notificationText);
-            this.panel1.Controls.Add(this.pictureBox7);
-            this.panel1.Controls.Add(this.panel4);
-            this.panel1.Controls.Add(this.panel3);
-            this.panel1.Location = new System.Drawing.Point(5, 78);
-            this.panel1.Margin = new System.Windows.Forms.Padding(4);
-            this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(777, 309);
-            this.panel1.TabIndex = 1;
-            // 
-            // notificationText
-            // 
-            this.notificationText.AutoSize = true;
-            this.notificationText.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.notificationText.Location = new System.Drawing.Point(332, 236);
-            this.notificationText.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
-            this.notificationText.Name = "notificationText";
-            this.notificationText.Size = new System.Drawing.Size(121, 20);
-            this.notificationText.TabIndex = 2;
-            this.notificationText.Text = "No notification ";
-            // 
-            // pictureBox7
-            // 
-            this.pictureBox7.Location = new System.Drawing.Point(357, 169);
-            this.pictureBox7.Margin = new System.Windows.Forms.Padding(4);
-            this.pictureBox7.Name = "pictureBox7";
-            this.pictureBox7.Size = new System.Drawing.Size(69, 50);
-            this.pictureBox7.TabIndex = 1;
-            this.pictureBox7.TabStop = false;
-            // 
-            // panel4
-            // 
-            this.panel4.BackColor = System.Drawing.Color.WhiteSmoke;
-            this.panel4.Controls.Add(this.buttonUnread);
-            this.panel4.Controls.Add(this.allButton);
-            this.panel4.Location = new System.Drawing.Point(1, 55);
-            this.panel4.Margin = new System.Windows.Forms.Padding(4);
-            this.panel4.Name = "panel4";
-            this.panel4.Size = new System.Drawing.Size(776, 66);
-            this.panel4.TabIndex = 0;
-            // 
-            // buttonUnread
-            // 
-            this.buttonUnread.Location = new System.Drawing.Point(73, 16);
-            this.buttonUnread.Margin = new System.Windows.Forms.Padding(4);
-            this.buttonUnread.Name = "buttonUnread";
-            this.buttonUnread.Size = new System.Drawing.Size(112, 38);
-            this.buttonUnread.TabIndex = 1;
-            this.buttonUnread.Text = "Unread";
-            this.buttonUnread.UseVisualStyleBackColor = true;
-            // 
-            // allButton
-            // 
-            this.allButton.BackColor = System.Drawing.Color.Black;
-            this.allButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.allButton.ForeColor = System.Drawing.Color.White;
-            this.allButton.Location = new System.Drawing.Point(12, 15);
-            this.allButton.Margin = new System.Windows.Forms.Padding(4);
-            this.allButton.Name = "allButton";
-            this.allButton.Size = new System.Drawing.Size(53, 39);
-            this.allButton.TabIndex = 0;
-            this.allButton.Text = "All";
-            this.allButton.UseVisualStyleBackColor = false;
-            // 
-            // panel3
-            // 
-            this.panel3.BackColor = System.Drawing.Color.Blue;
-            this.panel3.Controls.Add(this.pictureBox6);
-            this.panel3.Controls.Add(this.label21);
-            this.panel3.Location = new System.Drawing.Point(1, 0);
-            this.panel3.Margin = new System.Windows.Forms.Padding(4);
-            this.panel3.Name = "panel3";
-            this.panel3.Size = new System.Drawing.Size(776, 60);
-            this.panel3.TabIndex = 0;
-            // 
-            // pictureBox6
-            // 
-            this.pictureBox6.Location = new System.Drawing.Point(9, 7);
-            this.pictureBox6.Margin = new System.Windows.Forms.Padding(4);
-            this.pictureBox6.Name = "pictureBox6";
-            this.pictureBox6.Size = new System.Drawing.Size(55, 47);
-            this.pictureBox6.TabIndex = 1;
-            this.pictureBox6.TabStop = false;
-            // 
-            // label21
-            // 
-            this.label21.AutoSize = true;
-            this.label21.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label21.ForeColor = System.Drawing.Color.White;
-            this.label21.Location = new System.Drawing.Point(72, 15);
-            this.label21.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
-            this.label21.Name = "label21";
-            this.label21.Size = new System.Drawing.Size(164, 31);
-            this.label21.TabIndex = 0;
-            this.label21.Text = "Notifications";
-            // 
-            // panel10
-            // 
-            this.panel10.Controls.Add(this.label22);
-            this.panel10.Controls.Add(this.label23);
-            this.panel10.Location = new System.Drawing.Point(5, 4);
-            this.panel10.Margin = new System.Windows.Forms.Padding(4);
-            this.panel10.Name = "panel10";
-            this.panel10.Size = new System.Drawing.Size(777, 66);
-            this.panel10.TabIndex = 0;
-            // 
-            // label22
-            // 
-            this.label22.AutoSize = true;
-            this.label22.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label22.Location = new System.Drawing.Point(-4, 37);
-            this.label22.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
-            this.label22.Name = "label22";
-            this.label22.Size = new System.Drawing.Size(210, 18);
-            this.label22.TabIndex = 1;
-            this.label22.Text = " Daily tips and task notification ";
             // 
             // label23
             // 
             this.label23.AutoSize = true;
             this.label23.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label23.Location = new System.Drawing.Point(-5, 2);
-            this.label23.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label23.Location = new System.Drawing.Point(-4, 2);
             this.label23.Name = "label23";
-            this.label23.Size = new System.Drawing.Size(229, 31);
+            this.label23.Size = new System.Drawing.Size(185, 25);
             this.label23.TabIndex = 0;
             this.label23.Text = "Productivity Hub";
             // 
+            // label22
+            // 
+            this.label22.AutoSize = true;
+            this.label22.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label22.Location = new System.Drawing.Point(-3, 30);
+            this.label22.Name = "label22";
+            this.label22.Size = new System.Drawing.Size(62, 15);
+            this.label22.TabIndex = 1;
+            this.label22.Text = " Daily tips ";
+            // 
+            // panel10
+            // 
+            this.panel10.Controls.Add(this.label22);
+            this.panel10.Controls.Add(this.label23);
+            this.panel10.Location = new System.Drawing.Point(4, 3);
+            this.panel10.Name = "panel10";
+            this.panel10.Size = new System.Drawing.Size(583, 54);
+            this.panel10.TabIndex = 0;
+            // 
             // tipsContro_
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.Controls.Add(this.tipsPanel);
-            this.Margin = new System.Windows.Forms.Padding(4);
             this.Name = "tipsContro_";
-            this.Size = new System.Drawing.Size(817, 807);
+            this.Size = new System.Drawing.Size(599, 471);
             this.tipsPanel.ResumeLayout(false);
             this.tipsPanel.PerformLayout();
             this.flowLayoutPanel1.ResumeLayout(false);
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).EndInit();
-            this.panel1.ResumeLayout(false);
-            this.panel1.PerformLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox7)).EndInit();
-            this.panel4.ResumeLayout(false);
-            this.panel3.ResumeLayout(false);
-            this.panel3.PerformLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox6)).EndInit();
             this.panel10.ResumeLayout(false);
             this.panel10.PerformLayout();
             this.ResumeLayout(false);
@@ -307,15 +174,6 @@
         private System.Windows.Forms.Panel panel6;
         private System.Windows.Forms.Label label20;
         private System.Windows.Forms.PictureBox pictureBox5;
-        private System.Windows.Forms.Panel panel1;
-        private System.Windows.Forms.Label notificationText;
-        private System.Windows.Forms.PictureBox pictureBox7;
-        private System.Windows.Forms.Panel panel4;
-        private System.Windows.Forms.Button buttonUnread;
-        private System.Windows.Forms.Button allButton;
-        private System.Windows.Forms.Panel panel3;
-        private System.Windows.Forms.PictureBox pictureBox6;
-        private System.Windows.Forms.Label label21;
         private System.Windows.Forms.Panel panel10;
         private System.Windows.Forms.Label label22;
         private System.Windows.Forms.Label label23;
