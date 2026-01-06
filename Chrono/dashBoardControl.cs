568b7bbae16a5253745b08f1a06c8dd6016c6dbb
WenjaBoii <sir.rickraven14@gmail.com>
2026-01-06 23:48:07 +0800

    inayos form 1 then priorities update

--- Chrono/dashBoardControl.cs
+++ Chrono/dashBoardControl.cs
@@ -16,5 +16,40 @@ namespace Chrono
         {
             InitializeComponent();
         }
+
+        private void dashBoardControl_Load(object sender, EventArgs e)
+        {
+            RefreshDashboard();
+        }
+
+        pu
+        }
+
+        private void SetLabel(string name, string value)
+        {
+            var c = this.Controls.Find(name, true);
+            if (c.Length > 0 && c[0] is Label lbl) lbl.Text = value;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Use the Tasks tab to add tasks", "Info", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Use the Tasks tab to view all tasks", "Info", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+
+        private void label1_Click(object sender, EventArgs e) { }
+        private void label2_Click(object sender, EventArgs e) { }
+        private void panel3_Paint(object sender, PaintEventArgs e) { }
+        private void label6_Click(object sender, EventArgs e) { }
+        private void label7_Click(object sender, EventArgs e) { }
+        private void label9_Click(object sender, EventArgs e) { }
+        private void label11_Click(object sender, EventArgs e) { }
+        private void panel6_Paint(object sender, PaintEventArgs e) { }
+        private void panel7_Paint(object sender, PaintEventArgs e) { }
+        private void label23_Click(object sender, EventArgs e) { }
     }
 }
