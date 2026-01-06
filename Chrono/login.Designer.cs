fb8c6d41e777b81873e71f790482bad14f5559ca
Myshka Villavicencio <villavicenciomyshka@gmail.com>
2026-01-07 01:05:52 +0800

    register

--- Chrono/login.Designer.cs
+++ Chrono/login.Designer.cs
@@ -47,9 +47,9 @@
             // 
             this.label9.AutoSize = true;
             this.label9.Font = new System.Drawing.Font("Microsoft Sans Serif", 6.5F);
-            this.label9.Location = new System.Drawing.Point(49, 305);
+            this.label9.Location = new System.Drawing.Point(28, 305);
             this.label9.Name = "label9";
-            this.label9.Size = new System.Drawing.Size(276, 12);
+            this.label9.Size = new System.Drawing.Size(322, 13);
             this.label9.TabIndex = 16;
             this.label9.Text = "By signing in, you agree to our Terms of Service and Privacy Policy";
             // 
@@ -66,7 +66,7 @@
             this.panel1.Controls.Add(this.label3);
             this.panel1.Controls.Add(this.label2);
             this.panel1.Controls.Add(this.label1);
-            this.panel1.Location = new System.Drawing.Point(2, 3);
+            this.panel1.Location = new System.Drawing.Point(9, 3);
             this.panel1.Name = "panel1";
             this.panel1.Size = new System.Drawing.Size(362, 290);
             this.panel1.TabIndex = 15;
@@ -124,7 +124,7 @@
             this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label4.Location = new System.Drawing.Point(19, 136);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(75, 16);
+            this.label4.Size = new System.Drawing.Size(91, 20);
             this.label4.TabIndex = 4;
             this.label4.Text = "Password";
             // 
@@ -141,7 +141,7 @@
             this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label3.Location = new System.Drawing.Point(19, 89);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(78, 16);
+            this.label3.Size = new System.Drawing.Size(94, 20);
             this.label3.TabIndex = 2;
             this.label3.Text = "Username";
             // 
@@ -151,7 +151,7 @@
             this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F);
             this.label2.Location = new System.Drawing.Point(96, 51);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(172, 13);
+            this.label2.Size = new System.Drawing.Size(227, 17);
             this.label2.TabIndex = 1;
             this.label2.Text = "Sign in to your account to continue";
             // 
@@ -161,7 +161,7 @@
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 17F, System.Drawing.FontStyle.Bold);
             this.label1.Location = new System.Drawing.Point(91, 20);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(184, 29);
+            this.label1.Size = new System.Drawing.Size(219, 33);
             this.label1.TabIndex = 0;
             this.label1.Text = "Welcome back";
             // 
@@ -172,7 +172,7 @@
             this.Controls.Add(this.label9);
             this.Controls.Add(this.panel1);
             this.Name = "login";
-            this.Size = new System.Drawing.Size(366, 333);
+            this.Size = new System.Drawing.Size(382, 333);
             this.panel1.ResumeLayout(false);
             this.panel1.PerformLayout();
             this.ResumeLayout(false);
--- Chrono/register.cs
+++ Chrono/register.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using MySql.Data.MySqlClient;
 
 namespace Chrono
 {
@@ -17,6 +18,38 @@ namespace Chrono
             InitializeComponent();
         }
 
+        private void RegisterUser(string username, string email, string password)
+        {
+            using (var conn = DatabaseHelper.GetConnection())
+            {
+                conn.Open();
+
+                // CHECK for existing username or email
+                var checkCmd = new MySqlCommand(
+                    "SELECT COUNT(*) FROM users WHERE username = @u OR email = @e",
+                    conn);
+                checkCmd.Parameters.AddWithValue("@u", username);
+                checkCmd.Parameters.AddWithValue("@e", email);
+
+                int exists = Convert.ToInt32(checkCmd.ExecuteScalar());
+
+                if (exists > 0)
+                {
+                    MessageBox.Show("Username or email already exists.");
+                    return;
+                }
+
+                // INSERT new user
+                var insertCmd = new MySqlCommand(
+                    "INSERT INTO users (username, email, password) VALUES (@u, @e, @p)",
+                    conn);
+                insertCmd.Parameters.AddWithValue("@u", username);
+                insertCmd.Parameters.AddWithValue("@e", email);
+                insertCmd.Parameters.AddWithValue("@p", password);
+
+                insertCmd.ExecuteNonQuery();
+            }
+        }
         private void register_Load(object sender, EventArgs e)
         {
 
@@ -50,6 +83,32 @@ namespace Chrono
         private void linkLabel1_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
         {
 
+        }
+
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrWhiteSpace(usernameField.Text) ||
+    string.IsNullOrWhiteSpace(emailField.Text) ||
+    string.IsNullOrWhiteSpace(passwordField.Text))
+            {
+                MessageBox.Show("Please fill in all fields.");
+                return;
+            }
+
+            try
+            {
+                RegisterUser(usernameField.Text, emailField.Text, passwordField.Text);
+                MessageBox.Show("Account created! You may now sign in.");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Registration error: " + ex.Message);
+            }
         }
     }
 }
