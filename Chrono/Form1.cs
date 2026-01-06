4334e992e6311b57bf23a3619c8b70dfcada660b
WenjaBoii <sir.rickraven14@gmail.com>
2026-01-06 16:48:09 +0800

    may binago

--- Chrono/Form1.cs
+++ Chrono/Form1.cs
@@ -8,140 +8,142 @@
     using System.Threading.Tasks;
     using System.Windows.Forms;
 
-    namespace Chrono
+namespace Chrono
+{
+    public partial class Form1 : Form
     {
-        public partial class Form1 : Form
+
+        public Form1()
         {
+            InitializeComponent();
 
-            public Form1()
+
+        }
+
+        public void changePage(int pageNumber)
+        {
+            switch (pageNumber)
             {
-                InitializeComponent();
+                case 1:
 
- 
-            }
+                    break;
 
-            public void changePage(int pageNumber)
-            {
-                switch (pageNumber)
-                {
-                    case 1:
-                    
-                        break;
+                case 2:
 
-                    case 2:
-                  
-                        tasksControl1.BringToFront();
+                    tasksControl1.BringToFront();
 
-                        break;
+                    break;
 
-                    case 3:
+                case 3:
 
-                        prioritiesControl1.BringToFront();
-                        prioritiesControl1.SetSource(tasksControl1);
-                        prioritiesControl1.RefreshPriorityTask();
+                    prioritiesControl1.BringToFront();
+                    prioritiesControl1.SetSource(tasksControl1);
+                    prioritiesControl1.RefreshPriorityTask();
 
-                        break;
+                    break;
 
-                    case 4:
-                        focusTimerControl1.BringToFront();
-                        break;
-                    
-                    case 5:
-                        tipsContro_1.BringToFront();
-                        break;
-                    case 6:
-                        insightsControl1.BringToFront();
-                        break;
-                   
-                    default:
-                        pagePanel.Controls.Clear(); 
-                        break;        
-                }
-            }
+                case 4:
+                    focusTimerControl1.BringToFront();
+                    break;
 
-            private void label2_Click(object sender,EventArgs e)
-            {
+                case 5:
+                    tipsContro_1.BringToFront();
+                    break;
+                case 6:
+                    insightsControl1.BringToFront();
+                    break;
 
-            }
-
-            private void flowLayoutPanel2_Paint(object sender, PaintEventArgs e)
-            {
-
-            }
-
-            private void panel8_Paint(object sender, PaintEventArgs e)
-            {
-
-            }
-
-            private void label5_Click(object sender, EventArgs e)
-            {
-
-            }
-
-            private void panel14_Paint(object sender, PaintEventArgs e)
-            {
-
-            }
-
-            private void Form1_Load(object sender, EventArgs e)
-            {
-
-            }
-
-            private void panel7_Paint(object sender, PaintEventArgs e)
-            {
-
-            }
-
-            private void button7_Click(object sender, EventArgs e)
-            {
-
-            }
-
-            private void button5_Click(object sender, EventArgs e)
-            {
-                changePage(5);  
-            }
-
-            private void panel3_Paint(object sender, PaintEventArgs e)
-            {
-
-            }
-
-            private void panel15_Paint(object sender, PaintEventArgs e)
-            {
-
-            }
-
-            private void dashBoardNavButton_Click(object sender, EventArgs e)
-            {
-                changePage(1);  
-            }
-
-            private void focusTimerNavButton_Click(object sender, EventArgs e)
-            {
-                changePage(4);
-            }
-
-            private void taskNavButton_Click(object sender, EventArgs e)
-            {
-               changePage(2);
-            }
-
-            private void priorityNavButton_Click(object sender, EventArgs e)
-            {
-                changePage(3);
-            }
-
-            private void insightsNavButton_Click(object sender, EventArgs e)
-            {
-                changePage(6);
+                default:
+                    pagePanel.Controls.Clear();
+                    break;
             }
         }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void flowLayoutPanel2_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void panel8_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void label5_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void panel14_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void panel7_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            changePage(5);
+        }
+
+        private void panel3_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void panel15_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void dashBoardNavButton_Click(object sender, EventArgs e)
+        {
+            changePage(1);
+        }
+
+        private void focusTimerNavButton_Click(object sender, EventArgs e)
+        {
+            changePage(4);
+        }
+
+        private void taskNavButton_Click(object sender, EventArgs e)
+        {
+            changePage(2);
+        }
+
+        private void priorityNavButton_Click(object sender, EventArgs e)
+        {
+            changePage(3);
+        }
+
+        private void insightsNavButton_Click(object sender, EventArgs e)
+        {
+            changePage(6);
+        }
+
 
         private void pagePanel_Paint(object sender, PaintEventArgs e)
         {
 
         }
     }
+}
+    
